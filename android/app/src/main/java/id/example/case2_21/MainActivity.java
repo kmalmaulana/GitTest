f9f75d2fa3c1b0b2b6b2c061c77c56f48b19e727
kmalmaulana <akmalmaulana701@gmail.com>
2019-08-13 03:37:29 +0700

    Commit

---
+++ android/app/src/main/java/id/example/case2_21/MainActivity.java
@@ -0,0 +1,13 @@
+package id.example.case2_21;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
